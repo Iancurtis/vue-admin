948792cec14eb640d72828b9187125c3e6c52e2c
Fyn <811077@gmail.com>
2016-11-14 18:14:42 +0800

    添加目录结构文件

--- client/views/test/index.vue
+++ client/views/test/index.vue
@@ -1,10 +1,53 @@
 <template>
   <div>
+    <input type="number" id="name" v-model.number="msg">
+    <label for="name">{{msg}}</label>
+    <mycomponent
+      :foo='a'
+      :bar='msg'
+    >
+    </mycomponent>
+
+    <div class='show-test'>
+    <button v-on:click='show = !show'>toggle</button>
+    <!--<transition name='fade'>-->
+    <transition mode="out-in"
+        enter-active-class="fadeIn"
+        leave-active-class="fadeOut"
+        appear>
+      <p class='animated' v-show="show">I'm showing now</p>
+    </transition>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import mycomponent from './my-component'
 export default {
+  data () {
+    return {
+      show: false,
+      msg: '',
+      a: 'T',
+      b: 'F'
+    }
+  },
+  components: {
+    mycomponent
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+select {
+  background-color: red;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+</style>
